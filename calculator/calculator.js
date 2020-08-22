86f3395948298c55ce6c2e05cd6da07ce65ac2eb
Daniel Semple <dsemple.isu@gmail.com>
2020-08-21 20:16:28 -0500

    solved problems

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,29 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (arr) {
+	return arr.reduce((sum, currentValue) => sum + currentValue, 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((product, currentValue) => product * currentValue, 1);
 }
 
-function power() {
-	
+function power(x, y) {
+	return Math.pow(x, y);
 }
 
-function factorial() {
-	
+function factorial(x) {
+	if (x === 0) { 
+		return 1;
+	}	else {
+		return x *= factorial(x - 1);
+	}
 }
 
 module.exports = {
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function() {
+const palindromes = function(inPhrase) {
+    let palind = "";
+    let phrase = inPhrase.replace(/\W/g, "").toLowerCase();
+    for (let i = phrase.length - 1; i >= 0; i--) {
+        palind += phrase[i];
+    }
+    return palind == phrase;
 
 }
 
